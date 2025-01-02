b3ed5845ef821d0142d6d5949d19780b1d5a238b
Mrudula Godbole <149959221+mrudula-godbole@users.noreply.github.com>
2025-01-02 22:32:33 +0530

    Create basic-variable.js

---
+++ basic-variable.js
@@ -0,0 +1,16 @@
+const accountId=14453;
+let accountEmail="mrudula@google.com";
+var accountpassword="12345";
+accountCity="Pune";
+let accountState;
+// accountId=1;//Not Allowed
+console.log(accountId);
+accountCity="Mumbai";
+accountpassword="121";
+accountEmail="mrudulaaaa@google.com";
+console.log(accountCity);
+console.log(accountpassword);
+console.log(accountEmail);
+console.log(accountState);
+
+console.table([accountId,accountpassword,accountEmail,accountCity,accountState]);//Using table to show data in tabular format
