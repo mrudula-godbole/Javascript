4fcb9baba2e7312a959959dc70fe8f0b62cc5d02
Mrudula Godbole <149959221+mrudula-godbole@users.noreply.github.com>
2025-01-02 22:54:55 +0530

    Create datatype-Conversion.js

---
+++ datatype-Conversion.js
@@ -0,0 +1,18 @@
+let number="33";
+console.log(typeof number);
+console.log(typeof(number));
+let converttoNumber=Number(number);
+console.log(typeof converttoNumber);
+//While converting number "33"=>33
+//"33abc"=>NaN
+//true=1,false=>0
+let isLoggedin=1;
+let booleanisloggedIn=Boolean(isLoggedin);
+console.log(booleanisloggedIn);
+// 1=>true , 0=>false
+//""=> false
+//"mrudula"=>true
+let Num=33;
+let convertString=String(Num);
+console.log(Num);
+console.log(typeof convertString);
