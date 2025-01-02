a898a1b73a8241985e28c4581cafb4c739000c2c
Mrudula Godbole <149959221+mrudula-godbole@users.noreply.github.com>
2025-01-02 22:34:55 +0530

    Create datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,8 @@
+"use strict";//Strict all code as newer version
+//alert(3+3);We are using node js not browser
+// let name="Mrudula";// It is a String datatype
+let age=null;//int datatype
+let isLoggedIn=false;// boolean datatype
+console.log(typeof isLoggedIn);
+console.log(typeof age);
+//typeof is used to know datatype of variable
